ca01365489113197ed2545642ffd7acfa89538e9
John Mark Redding <johnmarkredding@gmail.com>
2018-11-07 16:10:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,8 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   response = ''
-  if (num % 3 != 0) && (num % 5 != 0)
+  if (num % 3 == 0) && (num % 5 == 0)
     nil
-  elsif 
+  elsif num % == 0
   end
 end
