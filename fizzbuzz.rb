a1c6cf47dc50b4b077beac3a999d1afade93f110
John Mark Redding <johnmarkredding@gmail.com>
2018-11-07 16:10:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  
+  response = ''
+  if (num % 3 != 0) && (num % 5 != 0)
+    nil
+  elsif 
+  end
 end
